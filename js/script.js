27761500994e503de7c83ea9048fa29d00200c09
Anita Korynek <gilkorynek@gmail.com>
2020-11-21 17:44:38 +0100

    Add randomNumber

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ var computerMove, playerMovie;
 playerMovie = 'papier';
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMovie + 'to wygrywasz!');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
