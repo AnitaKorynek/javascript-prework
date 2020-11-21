f69c729970646c4d8c5eb5327dd7a48ed82c6a16
Anita Korynek <gilkorynek@gmail.com>
2020-11-21 15:39:17 +0100

    Add structure js/script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+var computerMove, playerMovie;
+playerMovie = 'papier';
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMovie + 'to wygrywasz!');
